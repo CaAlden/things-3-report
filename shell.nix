2c25aabc6ae8adeddc0c3d47c60391610a39c957
Campbell Alden <campbellsalden@gmail.com>
2023-07-20 13:35:52 +0900

    Full swing off of python onto rust

--- shell.nix
+++ shell.nix
@@ -1,20 +1,8 @@
-with import <nixpkgs> {};
-with pkgs.python3Packages;
+{ pkgs ? import <nixpkgs> {} }:
 
-stdenv.mkDerivation {
-  name = "python";
-
+pkgs.mkShell {
   buildInputs = [
-    pip
-    python310
-    virtualenv
-  ];
-
-  shellHook = ''
-    SOURCE_DATE_EPOCH=$(date +%s)  # so that we can use python wheels
-    virtualenv --no-setuptools venv > /dev/null
-    export PATH=$PWD/venv/bin:$PATH > /dev/null
-    export PIP_DISABLE_PIP_VERSION_CHECK=1
-    pip install -r requirements.txt > /dev/null
-  '';
+    pkgs.rustc
+    pkgs.cargo
+  ] ++ [ pkgs.libiconv pkgs.darwin.apple_sdk.frameworks.CoreServices ];
 }
